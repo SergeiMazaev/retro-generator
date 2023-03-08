6cf96999526d97329473ca12300c7481a9a087c8
Sergey Mazaev <mazai.segr@gmail.com>
2023-03-08 11:35:38 +0300

    add select for group type

--- src/RetroGenerator.tsx
+++ src/RetroGenerator.tsx
@@ -3,10 +3,13 @@ import { teachersQuery } from './api/teachersQuery';
 import { CheckboxBlock } from './components/CheckboxBlock';
 import { groupsQuery } from './api/groupsQuery';
 import { Generator } from './components/Generator';
+import {Selector} from "./components/Selector";
+import {GroupType} from "./types";
 
 export const RetroGenerator = () => {
-  const [teachers] = createResource('online', teachersQuery,  { initialValue: [] });
-  const [groups] = createResource('online', groupsQuery, { initialValue: [] });
+  const [groupType, setGroupType] = createSignal<GroupType>('online');
+  const [teachers] = createResource(groupType, teachersQuery,  { initialValue: [] });
+  const [groups, {refetch}] = createResource(groupType, groupsQuery, { initialValue: [] });
   const [teacherCheck, setTeacherCheck] = createSignal<[string, boolean][]>([]);
   const [groupCheck, setGroupCheck] = createSignal<[string, boolean][]>([]);
 
@@ -34,8 +37,19 @@ export const RetroGenerator = () => {
     });
   };
 
+  const onGroupTypeChange = (value: string) => {
+    setGroupType(value as GroupType)
+    refetch();
+  }
+
   return (
     <div class="flex flex-col items-center m-auto w-3/4 my-5">
+      <Selector
+        title=""
+        values={['online', 'msk', 'spb']}
+        currentValue={groupType()}
+        onChange={onGroupTypeChange}
+      />
       <CheckboxBlock title="Преподаватели" values={teacherCheck()} onChange={onChangeTeacher} />
       <CheckboxBlock title="Группы студентов" values={groupCheck()} onChange={onChangeGroup} />
       <Generator
---
+++ src/components/Selector.tsx
@@ -0,0 +1,19 @@
+import {For, JSX} from "solid-js";
+
+type Props = {
+  title: string
+  values: string[],
+  currentValue: string,
+  onChange: (newValue: string) => void
+}
+
+export const Selector = (props: Props) => {
+  return <div>
+    <h2>{props.title}</h2>
+    <select value={props.currentValue} onChange={(e) => props.onChange(e.currentTarget.value)}>
+      <For each={props.values}>
+        {(value) => <option value={value}>{value}</option>}
+      </For>
+    </select>
+  </div>
+}
